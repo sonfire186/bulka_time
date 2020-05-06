62b7e019cf65d387a531220de10c75267af551b7
sonfire <igorsurgut95@gmail.com>
2020-05-06 21:02:18 +0500

    'init'

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var target_date = new Date(2020, 4, 5, 20, 10, 0, 0).getTime();
+
+const circleType = new CircleType(document.getElementById('bulka_text'));
+circleType.radius(500).dir(1);
+
+var days, hours, minutes, seconds;
+var countdown = document.getElementById("tiles");
+
+getCountdown();
+
+setInterval(function () {
+    getCountdown();
+}, 1000);
+
+function getCountdown() {
+
+    var current_date = new Date().getTime();
+    var seconds_left = (current_date - target_date) / 1000;
+
+    days = pad(parseInt(seconds_left / 86400));
+    seconds_left = seconds_left % 86400;
+
+    hours = pad(parseInt(seconds_left / 3600));
+    seconds_left = seconds_left % 3600;
+
+    minutes = pad(parseInt(seconds_left / 60));
+    seconds = pad(parseInt(seconds_left % 60));
+
+    countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+}
+
+function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+}
+
